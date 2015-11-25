6f12b14f8eec6b4173aefc462694b65c09b27139
Лилия Великова <lvelikova84@abv.bg>
2015-11-25 08:41:57 +0200

    модул 09, Видео 04 - Добавяне на клас за ескпорт към Excel

--- Interop/Excel/IOWrite.cs
+++ Interop/Excel/IOWrite.cs
@@ -9,6 +9,42 @@ namespace Excel
 		public IOWrite (DataStruct data)
 		{
 		}
+
+		public bool exportTable ()
+		{
+			try {
+
+				//Междинни проверки
+
+				return true;
+			} catch {
+			}
+
+			return false;
+		}
+
+			public void addRow (DataRow _row)
+			{
+				try{
+
+				}catch{
+				}
+			}
+
+			public void runFile ()
+			{
+				try{
+				System.Diagnostics.Process.Start (getPath ());
+						}catch{
+						}
+			}
+				
+
+			private string getPath ()
+			{
+			return System.IO.Path.Combine (AppDomain.CurrentDomain.BaseDirectory, "Table1.xlsx");
+				}
+
 	}
 }
 
